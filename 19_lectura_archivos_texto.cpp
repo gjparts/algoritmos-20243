007101480a529e397e3736d9e2d7e8f908aeffa5
gjparts <gjparts@gmail.com>
2024-10-10 11:45:16 -0600

    commit 1010202410208

--- 19_lectura_archivos_texto.cpp
+++ 19_lectura_archivos_texto.cpp
@@ -13,10 +13,31 @@ int main(){
 	
 	//validar si el archivo se pudo abrir
 	if( archivo.is_open() ){
+		int n = 0; //contador de lineas
+		
 		cout << "Se pudo abrir el archivo." << endl;
+		//recorrer el archivo linea por linea hasta llegar al final del mismo
+		//vamos a imprimir cada linea en la pantalla
+		string linea; //variable para depositar la linea recien leida en el archivo
+		//mientras el arxhivo no reporte eof() (END OF FILE) entonces vamos a repetir
+		while( !archivo.eof() ){
+			//!archivo.eof() es lo mismo que decir archivo.eof() == false
+			//para lograr llegar a eof hay que leer cada linea:
+			getline(archivo, linea);
+			cout << linea << endl; //imprimir la linea recien leida
+			n++;
+		}
+		cout << "Lineas en el archivo: " << n << endl;
+		//una vez terminamos de ocupar el archivo es una buena practica cerrarlo
+		archivo.close();
 	}
 	else
 		cout << "El archivo no existe o no tiene permiso para abrirlo." << endl;
 	
 	return 777;
 }
+
+
+
+
+
