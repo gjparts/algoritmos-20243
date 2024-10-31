17d059bf642e29c0ab93471a7c0557c3cdca2c1e
gjparts <gjparts@gmail.com>
2024-10-30 22:31:30 -0600

    commit 3010202412571

--- 27_ejercicios_funciones.cpp
+++ 27_ejercicios_funciones.cpp
@@ -11,5 +11,32 @@ int main(){
  	cout << esMultiplo(4,2) << endl;
  	cout << esMultiplo(5,3) << endl;
  	
+ 	//las funciones void al no retornar nada; entonces
+ 	//no se pueden imprimir ni tampoco asignar a variables.
+ 	//lo siguiente no funciona:
+ 	//cout << rectangulo(4,5,'x') << endl;
+ 	//lo correcto es:
+ 	rectangulo(4,5,'x');
+ 	cout << areaCirculo(14.0f) << endl; //para usar la version con float
+ 	cout << areaCirculo(14.0) << endl;  //para usar la version con double
+ 	
+ 	int a = 6,b = 7,c = 1;
+ 	cout << minimo(a,b,c) << endl; //prueba con int
+ 	
+ 	float q = 3.5, w = 6.7, e = 8.2;
+ 	cout << minimo(q,w,e) << endl; //prueba con float
+ 	
+ 	cout << minimo(5.8, 4.5655, 9.999) << endl; //prueba con double
+ 	cout << minimo(5.8f, 4.5655f, 9.999f) << endl; //prueba con float
+ 	cout << minimo(5, 4, 9) << endl; //prueba con int
+ 	
 	return 789;
 }
+
+
+
+
+
+
+
+
--- 27_funciones.h
+++ 27_funciones.h
@@ -1,6 +1,9 @@
 //aqui van las funciones
 #include<iostream>
 #include<stdexcept>
+#define _USE_MATH_DEFINES //se usa para que math.h o cmath
+						  //nos incluya las constantes matematicas
+						  //como por ejemplo PI, o logaritmos naturales
 #include<cmath>
 using namespace std;
 /*1. Escriba una funcion llamada elevar que reciba dos numeros enteros,
@@ -36,8 +39,58 @@ bool esMultiplo(int dividendo, int divisor){
 		return false;*/
 	return ( dividendo%divisor == 0 ? true : false );
 }
+/*4. Escriba una funcion que imprima un rectagulo de caracteres,
+dicha funcion recibira como parametros el ancho y largo del mismo
+asi como otro parametro con el caracter a usar para dibujar
+dicho rectangulo.*/
+void rectangulo(int ancho, int largo, char caracter){
+	for( int i = 1; i <= largo; i++ ){
+		for( int j = 1; j <= ancho; j++ ){
+			cout << caracter;
+		}
+		cout << endl;
+	}
+}
+/*5. Hacer una funcion que retorne el area de un circulo,
+dicha funcion debera recibir el radio como parametro.
+Usando sobrecarga de funciones haga que la funcion pueda
+trabajar con numeros double o con numeros float.*/
+double areaCirculo(double radio){
+	return M_PI*pow(radio,2); //M_PI sale de la libreria math
+}
+float areaCirculo(float radio){
+	return M_PI*pow(radio,2);
+}
 
-
+/*6. Escriba una función llamada minimo que retorne
+el menor de tres números, dicha funcion debera poder
+funcionar con tres numeros enteros o con
+tres numeros double o tres numeros float.
+Para evitar hacer lo mismo 3 veces con diferente tipo, se recomienda
+reutilizar la funcion que soporte numeros de mayor precision y mandarla
+a llamar desde las que usan numeros de menor precision*/
+double minimo(double a, double b, double c){
+	//de las tres sobrecargas de la funcion esta es la de mayor precision
+	if( a <= b && a <= c ) return a;
+	if( b <= a && b <= c ) return b;
+	if( c <= a && c <= b ) return c;
+}
+int minimo(int a, int b, int c){
+	//reutilizar la funcion de mayor precision
+	//para evitar que la funcion se llame a si misma
+	//se pasan los int a double para que se mande a llamar la version
+	//de la funcion de mayor precision (la que usa puros double)
+	double x = a, y = b, z = c;
+	return minimo(x,y,z);
+}
+float minimo(float a, float b, float c){
+	//reutilizar la funcion de mayor precision
+	//para evitar que la funcion se llame a si misma
+	//se pasan los int a double para que se mande a llamar la version
+	//de la funcion de mayor precision (la que usa puros double)
+	double x = a, y = b, z = c;
+	return minimo(x,y,z);
+}
 
 
 
