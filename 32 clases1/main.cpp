1b3d09a8d89c0721bc46ecebbfa1145da14eb161
gjparts <gjparts@gmail.com>
2024-11-12 11:49:42 -0600

    commit 1211202415775

---
+++ 32 clases1/main.cpp
@@ -0,0 +1,16 @@
+/*Pasos para manejar un proyecto de clases en C++:
+1) crear un folder para manejar nuestro proyecto con clases
+2) crear un archivo donde estara nuestra funcion main,
+   guardar dicho archivo como main.cpp dentro del folder creado
+   en el paso 1
+3) para cada una de las clases de su proyecto crear el respectivo
+   archivo nombrado con extension .h siempre dentro del mismo folder
+   creado en el paso 1 y hacer el include de dicho archivo .h en main.cpp*/
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	return 777;
+}
